091b3574999d8058fe9ddca2f7f0f8ff5c1df3b6
Yasindu <yasindumalmith058@gmail.com>
2026-02-07 18:37:24 +0530

    stop the eureka server

--- viago-admin-services/src/main/java/com/viago/adminservices/Main.java
+++ viago-admin-services/src/main/java/com/viago/adminservices/Main.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class Main {
 
     public static void main(String[] args) {
