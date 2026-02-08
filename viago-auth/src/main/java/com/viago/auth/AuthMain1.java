304a533f36f4644cea6da50a3b646eb65e38e826
Kaveengayanga1 <kaveengayanga@gmail.com>
2026-02-08 20:43:15 +0530

    Add slf4j to main class

--- viago-auth/src/main/java/com/viago/auth/AuthMain1.java
+++ viago-auth/src/main/java/com/viago/auth/AuthMain1.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@Slf4j
 public class AuthMain1 {
     public static void main(String[] args) {
         // Load environment variables from dev.env file
